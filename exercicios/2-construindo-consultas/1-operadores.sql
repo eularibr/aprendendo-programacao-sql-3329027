8b85edf0447725194a7423f60955747206d7abbd
Larissa Carvalho <99223888+eularibr@users.noreply.github.com>
2026-01-28 00:51:57 +0000

    resolucao1_part2

--- exercicios/2-construindo-consultas/1-operadores.sql
+++ exercicios/2-construindo-consultas/1-operadores.sql
@@ -1,6 +1,26 @@
 -- Construa uma consulta para a tabela "invoice_items" que retorne apenas as colunas InvoiceId, TrackId e UnitPrice.
+SELECT
+InvoiceId,
+TrackId,
+UnitPrice
+FROM
+invoice_items;
 
 -- Inclua nessa consulta mais três colunas, cada uma como resultado de uma operação aritmética diferente
+SELECT
+TrackId,
+UnitPrice,
+UnitPrice * 1.3,
+UnitPrice + 2.20,
+(UnitPrice + 2.20) % 2
+FROM
+invoice_items;
 
 -- Incremente essa consulta utilizando a cláusula WHERE, a qual restringirá o resultado apenas para linhas cujo o InvoiceId seja maior do que 20 e menor ou igual a 30
-
+SELECT
+TrackId,
+UnitPrice
+FROM
+invoice_items
+WHERE
+InvoiceId > 20 and InvoiceId <= 30;
